945a5537efa3683c012c0562703ccc6b358c3f59
Stijn Bernards <stijn@bernards.nl>
2018-02-08 23:46:32 +0100

    Fix migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,5 @@
 var GameToken = artifacts.require("./token/GameToken.sol");
 var AirDrop = artifacts.require("./AirDrop.sol");
 
-var dropper = "0xcBAC9e86E0B7160F1a8E4835ad01Dd51c514afce";
-
 module.exports = function(deployer) {
 };
